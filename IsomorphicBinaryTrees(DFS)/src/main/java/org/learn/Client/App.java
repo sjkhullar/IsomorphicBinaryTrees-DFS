485f48cc0495d6f77c7228e4248527b8774f5285
makeinjava <makeinjava@gmail.com>
2016-07-20 23:04:22 +0530

    Updated binary tree structure

--- IsomorphicBinaryTrees(DFS)/src/main/java/org/learn/Client/App.java
+++ IsomorphicBinaryTrees(DFS)/src/main/java/org/learn/Client/App.java
@@ -7,33 +7,33 @@ public class App
 {
 	public static void main( String[] args )
     {		
-		Node tree1 = Node.createNode(100);
-		tree1.left = Node.createNode(20);
-		tree1.right = Node.createNode(115);
+		Node tree1 = Node.createNode(50);
+		tree1.left = Node.createNode(30);
+		tree1.right = Node.createNode(30);
 		// left sub tree
-		tree1.left.left = Node.createNode(170);
-		tree1.left.right = Node.createNode(140);
+		tree1.left.left = Node.createNode(40);
+		tree1.left.right = Node.createNode(10);
 
 		// right subtree
 		tree1.right.left = Node.createNode(30);
-		tree1.right.right = Node.createNode(40);
+		tree1.right.right = Node.createNode(60);
 		
-		Node tree2 = Node.createNode(200);
-		tree2.left = Node.createNode(100);
-		tree2.right = Node.createNode(50);
+		Node tree2 = Node.createNode(10);
+		tree2.left = Node.createNode(40);
+		tree2.right = Node.createNode(20);
 		// left sub tree
-		tree2.left.left = Node.createNode(99);
-		tree2.left.right = Node.createNode(101);
+		tree2.left.left = Node.createNode(50);
+		tree2.left.right = Node.createNode(15);
 
 		// right subtree
-		tree2.right.left = Node.createNode(30);
-		tree2.right.right = Node.createNode(40);
+		tree2.right.left = Node.createNode(70);
+		tree2.right.right = Node.createNode(60);
 
 		boolean isSame = IsomorphicBinaryTree.isIsomorphicBinaryTree(tree1, tree2);
 		if(isSame) {
-			System.out.println("Binary Trees are Isomorphic");
+			System.out.println("Binary trees are Isomorphic");
 		} else {
-			System.out.println("Binary Trees are not Isomorphic");
+			System.out.println("Binary trees are not Isomorphic");
 		}
     }
 }
--- IsomorphicBinaryTrees(DFS)/src/main/java/org/learn/Question/IsomorphicBinaryTree.java
+++ IsomorphicBinaryTrees(DFS)/src/main/java/org/learn/Question/IsomorphicBinaryTree.java
@@ -1,21 +1,23 @@
-
 package org.learn.Question;
 
 public class IsomorphicBinaryTree {
+
 	public static boolean isIsomorphicBinaryTree(Node tree1, Node tree2) {
-		if(tree1 == null && tree2 == null) {
+
+		if (tree1 == null && tree2 == null) {
 			return true;
 		}
-		if(tree1 == null || tree2 == null) {
+
+		if (tree1 == null || tree2 == null) {
 			return false;
 		}
-		
-		if(false == isIsomorphicBinaryTree(tree1.left,tree2.left))
+
+		if (false == isIsomorphicBinaryTree(tree1.left, tree2.left))
 			return false;
-		
-		if(false ==  isIsomorphicBinaryTree(tree1.right,tree2.right))
+
+		if (false == isIsomorphicBinaryTree(tree1.right, tree2.right))
 			return false;
-		
+
 		return true;
 	}
 }
